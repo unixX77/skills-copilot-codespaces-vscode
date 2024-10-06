66c5e7cf923b280b0c92ccf7dc833183f2b705ad
unixX77 <113768102+unixX77@users.noreply.github.com>
2024-10-06 23:59:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-#data_customer_demo=pd.read_csv("C:\\Users\\GOURI SHANKAR KONDRA\\Downloads\\INTERN\\CustomerDemographics.csv")
+data_customer_demo=pd.read_csv("C:\\Users\\GOURI SHANKAR KONDRA\\Downloads\\INTERN\\CustomerDemographics.csv")
 data_retail1_=pd.read_csv("C:\\Users\\GOURI SHANKAR KONDRA\\Downloads\\INTERN\\Transactional_data_retail_01.csv")
 data_retail2_=pd.read_csv("C:\\Users\\GOURI SHANKAR KONDRA\\Downloads\\INTERN\\Transactional_data_retail_02.csv")
 
